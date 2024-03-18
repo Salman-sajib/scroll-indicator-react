669f36f0ccc79b7cd5d2d5a78b198d3dd22ca163
Salman-sajib <sfsajib@gmail.com>
2024-03-18 12:22:50 +0600

    added all the functionality and jsx for the scroll indicator

--- src/components/ScrollIndicator.jsx
+++ src/components/ScrollIndicator.jsx
@@ -4,6 +4,7 @@ function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [scrollProgress, setScrollProgress] = useState(0);
 
   async function fetchData(getUrl) {
     try {
@@ -20,15 +21,56 @@ function ScrollIndicator({ url }) {
     }
   }
 
-  console.log(data);
-
   useEffect(() => {
     fetchData(url);
   }, [url]);
 
+  function handleScrollProgress() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+
+    const howMuchScrolled = document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollProgress((howMuchScrolled / height) * 100);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScrollProgress);
+
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []);
+
+  console.log(data, scrollProgress);
+
+  if (loading) {
+    return (
+      <div>
+        <p>Loading... Please wait.</p>
+      </div>
+    );
+  }
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Custom Scroll Indicator</h1>
+      <div className='top-container'>
+        <h1>Custom Scroll Indicator</h1>
+        <div className='scroll-progress-container'>
+          <div
+            className='progress-bar'
+            style={{ width: `${scrollProgress}%` }}
+          ></div>
+        </div>
+      </div>
+
       <div className='data-container'>
         {data && data.length > 0
           ? data.map((dataItem) => <p key={dataItem.id}> {dataItem.title} </p>)
